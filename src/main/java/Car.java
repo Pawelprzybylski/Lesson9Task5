d4e8b935459f3158f3b175f8d42936b1f6f8cb69
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-08 09:24:03 +0200

    Commit2

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,15 +1,20 @@
 public class Car implements Vehicle {
+
+    private final Engine engine;
+
     public Car(Engine engine) {
-        engine.setIsON(engine.isOn());
+        this.engine = engine;
     }
 
     @Override
     public void drive() {
+        engine.setIsON(true);
         System.out.println("Jadę samochodem");
     }
 
     @Override
     public void stop() {
+        engine.setIsON(false);
         System.out.println("Zatrzymuję samochód");
     }
 
